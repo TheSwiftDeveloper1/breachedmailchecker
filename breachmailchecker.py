24f07e5bc59fcd712fe580e79b611274c3f9d6c7
Tri-dev.cyber <vhminhtrise@gmail.com>
2024-10-30 23:55:53 +0100

    Add files via upload

---
+++ breachmailchecker.py
@@ -0,0 +1,28 @@
+def wordcount(filename, email):
+    try:
+        with open(filename, "r") as file:
+            read = file.readlines()
+        
+        count = 0
+
+        for sentence in read:
+            line = sentence.split()
+            for each_word in line:
+                word = each_word.strip("/¤%()")
+                if email == word:
+                    count += 1
+
+        if count > 0:
+            print(f"{email} is breached {count} times.")
+        else:
+            print(f"{email} is not breached.")
+    
+    except FileNotFoundError:
+        print("The file is not found.")
+
+# Main loop for user input
+while True:
+    email_input = input("Enter email (or type 'exit' to quit): ")
+    if email_input.lower() == "exit":
+        break
+    wordcount("breachedmails.txt", email_input)
